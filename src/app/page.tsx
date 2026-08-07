f50715099aec46dd2b1ff958ed3a3827c47cd134
Jacqui Lough <lough.jacqui@gmail.com>
2026-08-07 10:40:19 +1000

    basic homepage at least links to the species list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,43 @@
-import Image from "next/image";
+import Link from "next/link";
+import { db } from "@/db";
+import { species } from "@/db/schema";
 
-export default function Home() {
+export default async function Home() {
+  const speciesList = await db.select().from(species);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="flex flex-1 flex-col bg-zinc-50 font-sans">
+      <main className="w-full flex-1 px-4 py-6 sm:px-6 sm:py-8 container mx-auto">
+        <h1 className="text-2xl font-semibold tracking-tight text-zinc-900 sm:text-3xl">
+          Australian H5N1 Bird Flu
+        </h1>
+        <p className="mt-1 text-md text-zinc-600">
+          Australia is notoriously far from many places in the world. Its
+          geographic isolation has led to it being home to many unique species
+          of wildlife. Up until recently, this isolation has protected it from
+          the decimation caused by H5N1 bird flu. However, as recent news
+          reports (links tk) have shown, bird flu has reached Australian shores,
+          and many of its animals are at risk. What are these species, and why
+          should we care? This site aims to track the species most at risk from
+          bird flu in Australia, providing information on their current
+          conservation status, h5n1 infection status, and ideally, awesome
+          photos of these guys.
+        </p>
+        <p className="mt-2 text-left italic text-md text-zinc-600">
+          As of Wednesday, 5 August 2026, there are {speciesList.length} species
+          tracked.
+        </p>
+        <p className="mt-2 text-md text-zinc-600">
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            <Link href="/species">View them all</Link>
+          </button>
+        </p>
+
+        <p className="mt-4 text-xs italic text-zinc-600">
+          note: all text is very very rough draft and I'd love to have a copy
+          desk at hand; this is a work in progress prototype by Jacqui Lough,
+          better flow and grammar is, hopefully, tk.
+        </p>
       </main>
     </div>
   );
--- src/app/species/page.tsx
+++ src/app/species/page.tsx
@@ -11,28 +11,6 @@ export default async function SpeciesPage() {
         <h1 className="text-2xl font-semibold tracking-tight text-zinc-900 sm:text-3xl">
           Australian Species at Risk from H5N1 Bird Flu
         </h1>
-        <p className="mt-1 text-sm text-zinc-600">
-          Australia is notoriously far from many places in the world. Its
-          geographic isolation has led to it being home to many unique species
-          of wildlife. Up until recently, this isolation has protected it from
-          the decimation caused by H5N1 bird flu. However, as recent news
-          reports (links tk) have shown, bird flu has reached Australian shores,
-          and many of its animals are at risk. What are these species, and why
-          should we care? This site aims to track the species most at risk from
-          bird flu in Australia, providing information on their current
-          conservation status, h5n1 infection status, and ideally, awesome
-          photos of these guys.
-        </p>
-        <p className="mt-1 text-sm text-zinc-600">
-          As of Wednesday, 5 August 2026, there are {speciesList.length} species
-          tracked.
-        </p>
-
-        <p className="mt-1 text-xs italic text-zinc-600">
-          note: all text is very very rough draft and I'd love to have a copy
-          desk at hand; this is a work in progress prototype by Jacqui Lough,
-          better flow and grammar is, hopefully, tk.
-        </p>
         <div className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {speciesList.map((s) => (
             <SpeciesCard key={s.id} species={s} />
